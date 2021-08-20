dbbf984df47a5f989e948a04ba77f5d9911830df
Mauricio Rinardo <mauricio.rinardo@gmail.com>
2021-08-20 09:54:22 -0300

    Adds seeds script

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,44 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias Rockelivery.{Item, Order, Repo, User}
+
+user = %User{
+  age: 27,
+  address: "Rua das Bananeiras, 15",
+  cep: "12345678",
+  cpf: "12345678991",
+  email: "mauricio@teste1.com",
+  password: "123456",
+  name: "Maurício Rinardo"
+}
+
+%User{id: user_id} = Repo.insert!(user)
+
+item1_data = %Item{
+  category: :food,
+  description: "Fígado acebolado",
+  price: Decimal.new("15.80"),
+  photo: "priv/photos/figado_acebolado.png"
+}
+
+item2_data = %Item{
+  category: :food,
+  description: "Feijoada completa",
+  price: Decimal.new("25.50"),
+  photo: "priv/photos/feijoada_completa.png"
+}
+
+item1 = Repo.insert!(item1_data)
+item2 = Repo.insert!(item2_data)
+
+order = %Order{
+  user_id: user_id,
+  items: [item1, item1, item2],
+  address: "Rua das Bananeiras, 15",
+  comments: "fígado ao ponto",
+  payment_method: :money
+}
+
+Repo.insert!(order)
